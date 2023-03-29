3eb7d2d07ee767e4b2313fad03582b1b6dbc6d1d
andyyuyc <76409596+andyyuyc@users.noreply.github.com>
2023-03-29 02:50:52 -0500

    Version 1.0.0

--- Jersey/JerseyApp.swift
+++ Jersey/JerseyApp.swift
@@ -35,34 +35,43 @@ class Jersey: Identifiable {
 
 struct ContentView: View {
     @State var jerseys = [Jersey]()
+    @State private var isShowingAddJerseyModal = false
 
     var body: some View {
         NavigationView {
-            ScrollView {
-                VStack {
-                    if jerseys.count > 0 {
-                        ForEach(jerseys) { jersey in
-                            JerseyRow(jersey: jersey)
-                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                                .padding(.vertical, 10)
-                        }
-                    } else {
-                        Text("You haven't added any jerseys yet.")
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
-                            .padding(.vertical, 300)
+            List {
+                if jerseys.count > 0 {
+                    ForEach(jerseys) { jersey in
+                        JerseyRow(jersey: jersey)
                     }
+                    .onDelete(perform: deleteJersey)
+                } else {
+                    Text("You haven't added any jerseys yet.")
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                        .padding(.vertical, 300)
                 }
             }
             .navigationBarTitle(Text("Jersey Collection"))
             .navigationBarItems(trailing: Button(action: {
-                let newJersey = Jersey(team: "New Team", player: "New Player", year: "New Year", image: UIImage(named: "defaultJersey")!)
-                jerseys.append(newJersey)
+                isShowingAddJerseyModal = true
             }) {
                 Image(systemName: "plus")
             })
+            .sheet(isPresented: $isShowingAddJerseyModal) {
+                AddJerseyView(jerseys: $jerseys)
+            }
         }
     }
+
+    private func deleteJersey(at offsets: IndexSet) {
+        jerseys.remove(atOffsets: offsets)
+    }
 }
+
+
+
+
+
 
 
 struct JerseyRow: View {
@@ -87,6 +96,7 @@ struct JerseyRow: View {
     }
 }
 
+
 struct DetailView: View {
     var jersey: Jersey
 
@@ -107,6 +117,113 @@ struct DetailView: View {
         .navigationBarTitle(Text(jersey.team), displayMode: .inline)
     }
 }
+
+
+struct AddJerseyView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var jerseys: [Jersey]
+
+    @State private var team: String = ""
+    @State private var player: String = ""
+    @State private var year: String = ""
+    @State private var image: UIImage? = UIImage()
+    @State private var isShowingImagePicker = false
+
+    var body: some View {
+        NavigationView {
+            Form {
+                // Other sections
+
+                Section(header: Text("Image")) {
+                    Button(action: {
+                        isShowingImagePicker = true
+                    }) {
+                        Text("Choose Image")
+                    }
+                    if let image = image {
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 200)
+                            .cornerRadius(10)
+                            .padding()
+                    }
+                }
+                Section(header: Text("Team")) {
+                    TextField("Enter team name", text: $team)
+                }
+
+                Section(header: Text("Player")) {
+                    TextField("Enter player name", text: $player)
+                }
+
+                Section(header: Text("Year")) {
+                    TextField("Enter year", text: $year)
+                }
+            }
+            .navigationBarTitle("Add Jersey")
+            .navigationBarItems(leading: Button("Cancel") {
+                presentationMode.wrappedValue.dismiss()
+            }, trailing: Button("Save") {
+                let newJersey = Jersey(team: team, player: player, year: year, image: image!)
+                jerseys.append(newJersey)
+                presentationMode.wrappedValue.dismiss()
+            })
+            .sheet(isPresented: $isShowingImagePicker, content: {
+                ImagePicker(selectedImage: $image, sourceType: .photoLibrary)
+            })
+        }
+    }
+}
+
+
+
+
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var selectedImage: UIImage?
+
+    var sourceType: UIImagePickerController.SourceType
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = context.coordinator
+        imagePicker.sourceType = sourceType
+        return imagePicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+
+
+
+
+
 
 
 struct ContentView_Previews: PreviewProvider {
