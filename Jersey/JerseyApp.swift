5caa506286418d0a521d9d93f7ac5336ac98c56c
andyyuyc <76409596+andyyuyc@users.noreply.github.com>
2023-03-28 19:14:41 -0500

    Initial Commit

---
+++ Jersey/JerseyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JerseyApp.swift
+//  Jersey
+//
+//  Created by Andy Yu on 2023/3/28.
+//
+
+import SwiftUI
+
+@main
+struct JerseyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
